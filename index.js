abf5900ff993459153cd5565e63ef40307e636e2
WinserEX <winser500@gmail.com>
2022-12-03 00:54:15 -0400

    Routes will be added in this commit

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ const express = require("express");
 const app = express();
 console.dir(app);
 
-app.use(() => {
-    console.log("New request");
+let test = 'request';
+
+//Allows you to work with requests and responses
+// app.use((req, res) => {
+//     console.log("New request");
+//     res.send(`<h1>${test}<h1>`);
+// })
+
+//Sets response for a specific route
+app.get('/', (req, res) => {
+    res.send("Home");
+ })
+
+app.get('/cats', (req, res) => {
+   res.send("Meow");
 })
 
+app.get('/dogs', (req, res) => {
+    res.send("woof");
+ })
+
+ //Post request
+ app.post('/cats', (req, res) => {
+    res.send("Rawwwr");
+ })
+
+ app.get('*', (req, res) => {
+    res.send("I don't know that path");
+ })
+
+// /cats => meow
+// /dogs => woof
+// '/'
+
+//Starts the server in the desired port
 app.listen(3000, ()=> {
     console.log("Listen to meeee");
 })
